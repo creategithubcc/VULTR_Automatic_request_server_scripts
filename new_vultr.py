c20bda98eb860721b82130010469e5476e899c59
KURANAGA YASUHIRO <71004114+creategithubcc@users.noreply.github.com>
2023-06-03 17:35:50 +0800

    Update new_vultr.py

--- new_vultr.py
+++ new_vultr.py
@@ -45,9 +45,14 @@ for i in range(0, 15):#循环创建15次
         print("创建失败，跳过！")
         continue
 
-    while True:
         try:
             breakserver(SUBID1)
         except:
-            print("诶呀删不掉，等60s再试一次！")
-            time.sleep(60)
+            while True:
+                print("还是删不掉，等20s后再试一次！")
+                time.sleep(20)
+                try:
+                    breakserver(SUBID1)
+                    break
+                except:
+                    continue
